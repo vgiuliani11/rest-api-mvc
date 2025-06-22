efa9cf711028cd48c7997593075b4a44885e5b78
Vinicius Giuliani <vinicius.giuliani@gmail.com>
2025-06-22 19:08:20 -0300

    fix: exports in product model should not be JSON.parsed

--- src/models/product.js
+++ src/models/product.js
@@ -1,27 +1,28 @@
 let products = [
   {
-    // id (Number),
-    // name (String)
-  }
+    id: 1,
+    name: 1
+  },
+  {}
 ]
 
 module.exports = {
   all() {
-    return JSON.parse({ route: 'all', products })
+    return { route: 'all', products }
   },
   create(id, name) {
-    return JSON.parse({ route: 'create', product: { id, name } })
+    return { route: 'create', product: { id, name } }
   },
   delete(id) {
-    return JSON.parse({ route: 'delete', product: products.find((p) => p.id === id) })
+    return { route: 'delete', product: products.find((p) => p.id === id) }
   },
   count() {
-    return JSON.parse({ route: 'count', count: products.length })
+    return { route: 'count', count: products.length }
   },
   findByName(name) {
-    return JSON.parse({ route: 'name', product: products.filter((p) => p.name.toLowerCase === name.toLowerCase) })
+    return { route: 'name', product: products.filter((p) => p.name.toLowerCase === name.toLowerCase) }
   },
   findByID(id) {
-    return JSON.parse({ route: 'id', product: products.filter((p) => p.id === id) })
+    return { route: 'id', product: products.filter((p) => p.id === id) }
   }
 }
