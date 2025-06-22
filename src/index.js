4d7320687935ba8bd9fd1bc312ed3dbcda8d1457
Vinicius Giuliani <vinicius.giuliani@gmail.com>
2025-06-22 19:59:28 -0300

    feat: implements routes for the api

--- src/index.js
+++ src/index.js
@@ -7,3 +7,12 @@ const PORT = 3000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
+
+const productController = require('./controllers/productController')
+
+app.get('/product/', productController.all)
+app.post('/product/', productController.create)
+app.delete('/product/:id', productController.delete)
+app.get('/product/count', productController.count)
+app.get('/product/name/:name', productController.findByName)
+app.get('/product/:id', productController.findByID)
